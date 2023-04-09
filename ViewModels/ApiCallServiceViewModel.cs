69676d6b54516dc8f832a24803ec3d45f247e3d6
xheljani <xheljan.laho@ikubinfo.al>
2023-04-09 03:29:45 +0200

    Donee

---
+++ ViewModels/ApiCallServiceViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace MvcCodeFlowClientManual.ViewModels
+{
+    public class ApiCallServiceViewModel
+    {
+        public IEnumerable<AccountViewModel> MyAccounts { get; set; }
+    }
+
+    public class AccountViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Classification { get; set; }
+        public string AccountSubType { get; set; }
+        public string AccountType { get; set; }
+        public string CurrentBalance { get; set; }
+        
+    }
+}
